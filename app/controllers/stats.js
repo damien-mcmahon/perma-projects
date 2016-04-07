820ef1577b4d7122b92e678004557220fc92246b
Damien McMahon <damien@macoto.co.uk>
2016-04-07 09:22:47 +0100

    Adds ability to update stats values. Will add admin only aspect to this button

--- app/controllers/stats.js
+++ app/controllers/stats.js
@@ -1,4 +1,34 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    setupStats(){
+      this.store.query('project', {
+        orderBy: 'id'
+      }).then((results) => {
+        let totalResults = results.get('length');
+        let statsModel = this.get('model').get('firstObject');
+        let countriesAdded = statsModel.get('projectsByCountry');
+
+        if(totalResults) {
+          countriesAdded = countriesAdded || {};
+          results.map((project) => {
+            let projectCountry = project.get('country');
+            if(!countriesAdded[projectCountry]) {
+              if(projectCountry){
+                countriesAdded[projectCountry] = 1;
+              }
+            } else {
+              if(projectCountry) {
+                let currentCount = countriesAdded[projectCountry];
+                countriesAdded[projectCountry] = ++currentCount;
+              }
+            }
+          });
+          statsModel.set('projectsByCountry', countriesAdded);
+          statsModel.save();
+        }
+      });
+    }
+  }
 });
