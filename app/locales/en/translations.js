088b6bc58ca4e2a5053d935f9cde5388e5a6d3f7
Damien McMahon <damien@macoto.co.uk>
2016-04-18 12:57:31 +0100

    Adds translations for english to about page and stats, cleaned up translation file structure too.

---
+++ app/locales/en/translations.js
@@ -0,0 +1,69 @@
+export default {
+  'app': {
+    'header': {
+      'subtitle': 'Permaculture Projects from around the world',
+      'nav': {
+        'about-page': 'About',
+        'stats-page': 'Stats',
+        'login-page': 'Sign In / Sign Up'
+      }
+    },
+  },
+  'index': {
+    'visible-projects': 'Visible Projects',
+    'search-box.placeholder': 'Search For Projects or Places'
+  },
+  'project-list' : {
+    'header': {
+      'title': 'Projects',
+      'search-box.placeholder': 'Start Typing to search',
+      'filter-button': {
+        'one': 'Filter',
+        'other': 'Filters'
+      }
+    },
+    'filter': {
+      'label': {
+        'wwoof': 'WWOOF Projects',
+        'produce': 'Sells Produce',
+        'eductional': 'Runs Educational Courses',
+        'visitors': 'Open to Visitors',
+        'volunteers': 'Looking for volunteers'
+      }
+    }
+  },
+  'about': {
+    'macoto': '<a href="http://macoto.co.uk">my personal site</a>',
+    'twitter': '<a href="https://twitter.com/perma_projects">@perma_projects</a>',
+    'github': '<a href="https://github.com/damien-mcmahon/perma-projects">source code for this site</a>',
+    'projectdata': '<a href="https://perma-project.firebaseio.com/projects.json">project data</a>',
+    'page-title': 'Welcome!',
+    'intro-paragraph': 'Perma Projects is a site that\'s intended to help anyone looking to find Permaculture projects in their location.There are many disparate sites each with their own website and directories of places where Permaculture is being used. Perma Projects will hopefully become a place where it\'s possible to find all of these projects in one place.',
+    'help-title': 'Get Involved',
+    'help-paragraph': 'I\'ve created this site as I\'m interested in visiting, volunteering and finding out where Permaculture is happening near me and I decided to create a resource that others can use too. If you run a permaculture project and would like to share it then please feel free to sign up (login via Facebook) and it\'s very easy to add your project.',
+    'get-data-title': 'Get the Data',
+    'get-data-github': 'In the spirit of permaculture and sharing, the {{{githubLink}}} is freely available (meaning you can clone this site).',
+    'get-project-data-link': 'The {{{projectDataLink}}} is freely available to use as you see fit and perhaps make something interesting with.',
+    'get-in-touch-title': 'Get in touch',
+    'get-in-touch-macoto': 'If you\'d like to get in contact with me, visit {{{macotoLink}}} for more info!.',
+    'get-in-touch-twitter': 'Or via Twitter: {{{twitterLink}}}',
+    'data-sources-title': 'Data Sources used',
+  },
+  'stats': {
+    'title': 'PermaProject statistics',
+    'intro-text': 'Here are the current totals for Perma Projects. If your project is missing or your country has a small number of projects, sign in to add your project and let others know about your work.',
+    'stat': {
+      'totalProjects': 'Total Permaculture Projects',
+      'totalUsers': 'Total Users on PermaProjects.com'
+    }
+  }
+  // "some.translation.key": "Text for some.translation.key",
+  //
+  // "a": {
+  //   "nested": {
+  //     "key": "Text for a.nested.key"
+  //   }
+  // },
+  //
+  // "key.with.interpolation": "Text with {{anInterpolation}}"
+};
