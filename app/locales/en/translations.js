bad534ef623c092da4c07c44ac22cd04bdab240b
Damien McMahon <damien@macoto.co.uk>
2016-04-19 14:19:01 +0100

    Adds Google translated french content

--- app/locales/en/translations.js
+++ app/locales/en/translations.js
@@ -57,13 +57,4 @@ export default {
       'totalUsers': 'Total Users on PermaProjects.com'
     }
   }
-  // "some.translation.key": "Text for some.translation.key",
-  //
-  // "a": {
-  //   "nested": {
-  //     "key": "Text for a.nested.key"
-  //   }
-  // },
-  //
-  // "key.with.interpolation": "Text with {{anInterpolation}}"
 };
--- app/locales/fr/translations.js
+++ app/locales/fr/translations.js
@@ -1,11 +1,60 @@
 export default {
-  // "some.translation.key": "Text for some.translation.key",
-  //
-  // "a": {
-  //   "nested": {
-  //     "key": "Text for a.nested.key"
-  //   }
-  // },
-  //
-  // "key.with.interpolation": "Text with {{anInterpolation}}"
+  'app': {
+    'header': {
+      'subtitle': 'Permaculture projets du monde entier',
+      'nav': {
+        'about-page': 'About',
+        'stats-page': 'Statistiques',
+        'login-page': 'Se connecter / S\'inscrire'
+      }
+    },
+  },
+  'index': {
+    'visible-projects': 'Projets visibles',
+    'search-box.placeholder': 'Rechercher des projets ou des lieux'
+  },
+  'project-list' : {
+    'header': {
+      'title': 'Projets',
+      'search-box.placeholder': 'Commencez à taper à la recherche',
+      'filter-button': {
+        'one': 'filtre',
+        'other': 'filtres'
+      }
+    },
+    'filter': {
+      'label': {
+        'wwoof': 'Projets WWOOF',
+        'produce': 'Propose à vendre Produce',
+        'eductional': 'Exécute Cours d\'éducation',
+        'visitors': 'Ouvert aux visiteurs',
+        'volunteers': 'Recherche de volontaires'
+      }
+    }
+  },
+  'about': {
+    'macoto': '<a href="http://macoto.co.uk">mon site personnel</a>',
+    'twitter': '<a href="https://twitter.com/perma_projects">@perma_projects</a>',
+    'github': '<a href="https://github.com/damien-mcmahon/perma-projects">Code source pour ce site</a>',
+    'projectdata': '<a href="https://perma-project.firebaseio.com/projects.json">données du projet</a>',
+    'page-title': 'Bienvenue!',
+    'intro-paragraph': 'Perma Projects est un site qui a pour but d\'aider ceux qui cherchent à trouver des projets permaculture dans leur location.There sont nombreux sites disparates chacun avec leur propre site Web et répertoires des endroits où Permaculture est utilisé. Perma projets, nous l\'espérons devenir un endroit où il est possible de trouver tous ces projets en un seul endroit.',
+    'help-title': 'Être impliqué',
+    'help-paragraph': 'J\'ai créé ce site que je suis intéressé à visiter, faire du bénévolat et de découvrir où Permaculture se passe près de moi et je décide de créer une ressource que les autres peuvent utiliser aussi. Si vous exécutez un projet de permaculture et que vous souhaitez partager, puis se sentir s\'il vous plaît libres de vous inscrire (connecter via Facebook) et il est très facile d\'ajouter votre projet.',
+    'get-data-title': 'Obtenez les données',
+    'get-data-github': 'Dans l\'esprit de la permaculture et de partage, le {{{githubLink}}} est disponible gratuitement (ce qui signifie que vous pouvez cloner ce site).',
+    'get-project-data-link': 'Le {{{projectDataLink}}} est disponible gratuitement à utiliser comme bon vous semble et peut-être faire quelque chose d\'intéressant avec.',
+    'get-in-touch-title': 'Entrer en contact',
+    'get-in-touch-macoto': 'Si vous souhaitez entrer en contact avec moi, visite {{{macotoLink}}} pour plus d\'info!',
+    'get-in-touch-twitter': 'Ou via Twitter: {{{twitterLink}}}',
+    'data-sources-title': 'Sources de données utilisées',
+  },
+  'stats': {
+    'title': 'Statistiques Perma projet',
+    'intro-text': 'Voici les totaux actuels pour Perma projets. Si votre projet est manquant ou votre pays a un petit nombre de projets, connectez-vous pour ajouter votre projet et faites connaître votre travail.',
+    'stat': {
+      'totalProjects': 'Projets Total des Permaculture',
+      'totalUsers': 'Nombre total d\'utilisateurs sur PermaProjects.com'
+    }
+  }
 };
