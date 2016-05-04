525b149f686bc6c0a8586a275505c3baa81e2c7e
Damien McMahon <damien@macoto.co.uk>
2016-05-04 17:11:04 +0100

    Some tweaks to placeholder texts

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  displayName: DS.attr('string', {defaultValue: 'Unamed User'}),
+  displayName: DS.attr('string'),
   loginId: DS.attr('string'),
   profileImageUrl: DS.attr('string'),
   email: DS.attr('string'),
